358cbf46b6bd22c99aa00ba77ae80ee6fd960853
Philip Schlump <pschlump@gmail.com>
2026-08-14 11:56:01 -0600

    Version Bump

--- version.go
+++ version.go
@@ -1,10 +1,10 @@
 
 package main
 
-var GitCommit string = `9798655f757e82f70943ab8e85d1d52499228383
+var GitCommit string = `168dd800e5cdc576e7238dcc52ca341fcfc8d1cd
 Tag: 
-v1.0.15
+v1.0.16
 Build Date: 
-Fri Aug 14 11:41:13 MDT 2026
+Fri Aug 14 11:56:00 MDT 2026
 `
 
